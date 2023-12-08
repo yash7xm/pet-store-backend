3c1b5230c52f4ba9d98509b56c868faeed5bb581
yash7xm <yash.7xm@gmail.com>
2023-12-08 13:03:54 +0530

    db connected

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 import Express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+dotenv.config({
+    path: '.env'
+})
+
+mongoose.connect(process.env.MONGO_PROD_URL)
+    .then(() => console.log('connected db'))
+    .catch(err => console.log(err));
+
 const app = Express();
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
 
 app.get('/', (req, res) => {
     res.send('root')
